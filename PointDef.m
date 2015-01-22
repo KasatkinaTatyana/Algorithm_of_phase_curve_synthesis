040b7e3f6291abd252bd3c4e0b230d4c9d30c566
Tatyana Kasatkina <kasatkina_t_s@mail.ru>
2015-01-22 23:29:13 +0300

    Initial commit.
    Алгоритм построения фазовой кривой. Другой вариант алгоритма,
    содержащегося в репозитории
    Advanced-algorithm-of-phase-trajectory-synthesis.

---
+++ PointDef.m
@@ -0,0 +1,55 @@
+function F = PointDef(Y_0, Y_end, N, d)
+global constr
+
+delta = Y_end(1) - Y_0(1);
+
+c0 = Y_0(2);
+c1 = Y_0(3)/Y_0(2);
+
+Ma = [delta^2     delta^3;
+      2*delta     3*delta^2];
+  
+Mb = [Y_end(2) - c1*delta - c0;
+      Y_end(3)/Y_end(2) - c1];
+Mc = inv(Ma)*Mb;
+
+c2 = Mc(1);
+c3 = Mc(2);
+
+dy = (Y_end(1) - Y_0(1))/N;
+
+y=Y_0(1):dy:Y_end(1);
+Psi = c0 + c1*(y - Y_0(1)) + c2*(y - Y_0(1)).^2 + c3*(y - Y_0(1)).^3+ ...
+            d*((y - Y_0(1)).^2).*(y - Y_end(1)).^2;
+
+
+flag = 0;
+for i=1:(N+1)
+    if ((flag == 0)&&(Psi(i) >= constr))
+        i_1 = floor((i - 1)/2);
+        
+        flag = 1;
+    end
+    if ((flag==1)&&(Psi(i) < constr))
+        i_2 = floor((N + 1  - i)/2 + i);
+        break;
+    end
+end
+
+if (flag == 1)
+    y_left(1) = Y_0(1) + i_1*dy;  
+    y_left(2) = Psi(i_1);
+    y_left(3) = (c1 + 2*c2*(y_left(1) - Y_0(1)) + 3*c3*(y_left(1) - Y_0(1))^2 + ...
+            2*d*(y_left(1) - Y_0(1))*(y_left(1) - Y_end(1))^2 + ...
+            2*d*(y_left(1) - Y_0(1))^2*(y_left(1) - Y_end(1)) )*y_left(2);
+        
+    y_right(1) = Y_0(1) + i_2*dy;
+    y_right(2) = Psi(i_2);
+    y_right(3) = (c1 + 2*c2*(y_right(1) - Y_0(1)) + 3*c3*(y_right(1) - Y_0(1))^2 + ...
+            2*d*(y_right(1) - Y_0(1))*(y_right(1) - Y_end(1))^2 + ...
+            2*d*(y_right(1) - Y_0(1))^2*(y_right(1) - Y_end(1)) )*y_right(2);
+        
+    F = [y_left; y_right];
+else 
+    F = 0;
+end
---
+++ find_borders.m
@@ -0,0 +1,50 @@
+function F = find_borders(Y_0, Y_end, N, N_shift_left, N_shift_right, d)
+global constr
+
+delta = Y_end(1) - Y_0(1);
+
+c0 = Y_0(2);
+c1 = Y_0(3)/Y_0(2);
+
+Ma = [delta^2     delta^3;
+      2*delta     3*delta^2];
+  
+Mb = [Y_end(2) - c1*delta - c0;
+      Y_end(3)/Y_end(2) - c1];
+Mc = inv(Ma)*Mb;
+
+c2 = Mc(1);
+c3 = Mc(2);
+
+dy = (Y_end(1) - Y_0(1))/N;
+
+y=Y_0(1):dy:Y_end(1);
+Psi = c0 + c1*(y - Y_0(1)) + c2*(y - Y_0(1)).^2 + c3*(y - Y_0(1)).^3+ ...
+            d*((y - Y_0(1)).^2).*(y - Y_end(1)).^2;
+
+
+flag = 0;
+for i=1:(N+1)
+    if ((flag == 0)&&(Psi(i) >= constr))
+        y_left(1) = Y_0(1) + (i-1 - N_shift_left)*dy;  % отступили N_shift шагов назад от критической точки
+        y_left(2) = Psi(i - 1 - N_shift_left);
+        y_left(3) = (c1 + 2*c2*(y_left(1) - Y_0(1)) + 3*c3*(y_left(1) - Y_0(1))^2 + ...
+            2*d*(y_left(1) - Y_0(1))*(y_left(1) - Y_end(1))^2 + ...
+            2*d*(y_left(1) - Y_0(1))^2*(y_left(1) - Y_end(1)) )*y_left(2);
+        flag = 1;
+    end
+    if ((flag==1)&&(Psi(i) < constr))
+        y_right(1) = Y_0(1) + (i + N_shift_right)*dy;
+        y_right(2) = Psi(i + N_shift_right);
+        y_right(3) = (c1 + 2*c2*(y_right(1) - Y_0(1)) + 3*c3*(y_right(1) - Y_0(1))^2 + ...
+            2*d*(y_right(1) - Y_0(1))*(y_right(1) - Y_end(1))^2 + ...
+            2*d*(y_right(1) - Y_0(1))^2*(y_right(1) - Y_end(1)) )*y_right(2);
+        break;
+    end
+end
+
+if (flag == 1)
+    F = [y_left; y_right];
+else 
+    F = 0;
+end
